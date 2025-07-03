cee7b8993ea9e80188c8c0cd11ea6d06afc44d38
Sneha <bhardwajsneha111@gmail.com>
2025-07-03 12:46:26 +0530

    fix appStore

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,44 +1,14 @@
-// utils/appStore.js
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from './userSlice.js';
-import feedReducer from './feedSlice.js';
-import requestReducer from './requestSlice.js';
-import connectionReducer from './connectionSlice.js';
-
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER
-} from 'redux-persist';
-
-import storage from 'redux-persist/lib/storage'; // Uses localStorage by default
-
-// Persist config for user slice
-const userPersistConfig = {
-  key: 'user',
-  storage,
-};
-
-const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
-
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from   "./userSlice.js"
+import feedReducer from "./feedSlice.js"
+import requestReducer from "./requestSlice.js"
+import connectionReducer from "./connectionSlice.js"
 export const appStore = configureStore({
   reducer: {
-    user: persistedUserReducer,
+    user: userReducer,
     feed: feedReducer,
     connections: connectionReducer,
     requests: requestReducer,
-  },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-});
 
-export const persistor = persistStore(appStore);
+  },
+})
