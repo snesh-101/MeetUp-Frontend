c38295dddf7e75a07f00051aacdaf65be7e46943
Sneha <bhardwajsneha111@gmail.com>
2025-07-06 11:57:16 +0530

    update main

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,12 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import App from './App.jsx'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App />
-//   </StrictMode>,
-// )
-// main.jsx or index.jsx
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { Provider } from "react-redux";
-import { appStore, persistor } from "./utils/appStore";
-// import { PersistGate } from "redux-persist/integration/react";
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={appStore}>
-    <PersistGate loading={null} persistor={persistor}>
-      <App />
-    </PersistGate>
-  </Provider>
-);
+
